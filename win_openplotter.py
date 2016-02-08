31208d6ef4337125260e40784063bfd13dfc4a3a
gypsylyon <gypsylyon@gmail.com>
2016-02-08 18:04:02 +0100

    Update win_openplotter.py

--- win_openplotter.py
+++ win_openplotter.py
@@ -1,3 +1,14 @@
+#                Anemometro de Hilo Caliente 20/12/15  (Hot wire anemometer)
+# 		 This project was developed to use with Openplotter
+#                Cuatro sensores MD0550 dxf de Modern Device(Four sensors  MD0550 dxf from Modern Device)   
+#                Raspberry Pi2 B 1.1 and  A/D converter MCP 3008 with 10 bits/ 3,3 Voltios
+#                Conexion via GPIO por SPI (Serial Paralel Interface)  Simulado por Sofware
+#		 First part: read analog data from MCP3008 or MCP3208
+#		 second part: Calculation of Win vector and win direcction
+#		 third part: send data tu Openplotter via localhost
+
+
+
 import time;
 import math
 import RPi.GPIO as GPIO
@@ -14,7 +25,7 @@ config = ConfigParser()
 # parse existing file
 config.read('win_openplotter.ini')
 
-
+# Beginn script to data read of the ADC MCP3008 or MCP3208 - from Erik Bartmann -www.erik-bartmann.de
 GPIO.setmode(GPIO.BCM)  # GPIO-Pin Bezeichnungen verwenden
 GPIO.setwarnings(False) # Warnungen deaktivieren
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -39,7 +50,7 @@ def readAnalogData(adcChannel, SCLKPin, MOSIPin, MISOPin, CSPin, delay):
         sendCMD <<= 1 # Bitfolge eine Position nach links schieben
     # Empfangen der Daten des AD-Wandlers
     adcValue = 0 # Reset des gelesenen Wertes
-    for i in range(11):
+    for i in range(11): # 13 bei MCP3208
         # Negative Flanke des Clock-Signals generieren
         GPIO.output(SCLKPin, GPIO.HIGH)
         GPIO.output(SCLKPin, GPIO.LOW)
@@ -57,7 +68,7 @@ def setupGPIO(SCLKPin, MOSIPin, MISOPin, CSPin):
     GPIO.setup(CSPin,   GPIO.OUT)
 
 # Variablendefinition ( read values from a section)
-
+# Variable to read Analog data
 ADCChannel = config.get('section_a', 'ADCChannel') # AD-Kanal
 SCLK = config.getint('section_a', 'SCLK') # Serial-Clock
 MOSI = config.getint('section_a', 'MOSI')  # Master-Out-Slave-In
@@ -66,6 +77,10 @@ CS = config.getint('section_a', 'CS') # Chip-Select
 
 PAUSE = config.getfloat('section_a', 'PAUSE')
 Ref_volt = config.getfloat('section_a', 'Ref_volt')
+# End of variable to read analog data
+
+# Begin of variable to use in Anemometer
+
 Temp_cali = config.get('section_a', 'Temp_cali')
 
 con_temp0_a = config.getfloat('section_a', 'con_temp0_a')
@@ -85,10 +100,13 @@ con_win_mph0_b = config.getfloat('section_a', 'con_win_mph0_b')
 con_win_mph1_b = config.getfloat('section_a', 'con_win_mph1_b')
 con_win_mph2_b = config.getfloat('section_a', 'con_win_mph2_b')
 con_win_mph3_b = config.getfloat('section_a', 'con_win_mph3_b')
+ # end of variable to use in Anemometer
 
+setupGPIO(SCLK, MOSI, MISO, CS) # GPIO-Pin Setup and end of script to data  read
+# Use of the funktion to read Analog data " readAnalogData(0, SCLK, MOSI, MISO, CS, PAUSE)"
 
-setupGPIO(SCLK, MOSI, MISO, CS) # GPIO-Pin Setup
-
+# Begin script to Anemometer from Gypsylyon
+# Calibration in customice Wincanal. Reference Anemometer VOLTCRAFT BL-30 AN error 0,2m/s
 
 while True:
    Cont = 1
@@ -141,7 +159,7 @@ while True:
 
    Win_MPH_0 = con_win_mph0_a * math.pow(Win_Inter, con_win_mph0_b) 
 
-# Calculos Canal1
+# Calculos Canal 1
    Win_V_1 = Win_D_1 * Ref_volt
    Temp_V_1 = Temp_D_1 * Ref_volt
    Temp_1 = con_temp0_a * math.pow(Temp_V_1, 2) - con_temp0_b * Temp_V_1 + con_temp0_c
@@ -178,6 +196,7 @@ while True:
 
 
 # Calculos Vector y direccion viento
+# Detection of the cuadrant
 
    if (Win_MPH_0 > Win_MPH_1 and Win_MPH_0 > Win_MPH_2 and Win_MPH_0 > Win_MPH_3 ): 
 
@@ -238,7 +257,7 @@ while True:
            Win_B = Win_MPH_0
            Cuadrante = 7
 
-
+# Calculation of the angle in cuadrant
    Rel1 = Win_A / Win_B
 
    if (Rel1 < 1.05):
@@ -251,7 +270,7 @@ while True:
    
    Direccion = (-0.00000000000000007 + Raiz_bac) / (2 * 0.0003)
 
-  
+# Calculation of the angle refered to 360 degres
    if (Cuadrante == 1):
        W_Direccion = 360 - Direccion
 
@@ -284,8 +303,7 @@ while True:
        W_Direccion = 270 + Direccion
 
 
-  #   Vector_1 = 0.0003 * Direccion * Direccion - 0.00000000000000007 * Direccion + 1.0441
-  
+# Calculation of the value of the win vector
 
    V_1 = math.cos(45) * math.sqrt((1 + (0.04 * 0.04)) * Win_B * Win_B - (0.04 * 0.04) * Win_A * Win_A) 
    V_2 = math.cos(45) * math.sqrt((1 + (0.04 * 0.04)) * Win_A * Win_A - (0.04 * 0.04) * Win_B * Win_B)
@@ -293,12 +311,15 @@ while True:
 
    Win_MPH_Vector =  ((V_1 + V_2) * 1.18) * 0.868976
 
-   T_Media = (Temp_0 + Temp_1 + Temp_2 + Temp_3) / 4
-
+   T_Media = (Temp_0 + Temp_1 + Temp_2 + Temp_3) / 4   # Midle temparature of 4 sensors
+   
+   # Cnversion tu NMEA0183
    mwv = pynmea2.MWV('OS', 'MWV', (str(round(W_Direccion,1)),'R',str(round(Win_MPH_Vector,1)),'N','A'))
    mwv1=str(mwv)
    mwv2=mwv1+"\r\n"
-   sock.sendto(mwv2, ('localhost', 10110))
+   sock.sendto(mwv2, ('localhost', 10110)) # Send datatu localhost 10110 to allow data read with Openplotter
+   
+   # Control show data  (not necessry to used with Openplotter
  
    print '                      Anemometro de Hilo Caliente 20/12/15      '
    print '                      Cuatro sensores MD0550 dxf de Modern Device        '       
